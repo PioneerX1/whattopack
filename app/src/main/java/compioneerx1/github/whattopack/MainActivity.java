90a6a4d9ff6754e2316881c17fea571245dbd145
Mick Sexton <lacrookedbeat@hotmail.com>
2017-09-08 09:23:55 -0700

    add custom font - pacifico

--- app/src/main/java/compioneerx1/github/whattopack/MainActivity.java
+++ app/src/main/java/compioneerx1/github/whattopack/MainActivity.java
@@ -1,13 +1,21 @@
 package compioneerx1.github.whattopack;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface pacificoFont = Typeface.createFromAsset(getAssets(), "fonts/pacifico.ttf");
+        mAppNameTextView.setTypeface(pacificoFont);
     }
 }
