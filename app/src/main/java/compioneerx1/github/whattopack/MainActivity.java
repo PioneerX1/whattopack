f82673a0eb85b3eff8e3ba7e2c69cd43802f79aa
Mick Sexton <lacrookedbeat@hotmail.com>
2017-09-08 10:19:34 -0700

    add date input activity

--- app/src/main/java/compioneerx1/github/whattopack/MainActivity.java
+++ app/src/main/java/compioneerx1/github/whattopack/MainActivity.java
@@ -1,21 +1,45 @@
 package compioneerx1.github.whattopack;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mAppNameTextView;
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private TextView mAppNameTextView;   // for custom font
+    private Button mLocationButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mLocationButton = (Button) findViewById(R.id.locationButton);
+        mLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
+                Intent intent = new Intent(MainActivity.this, DatesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Custom font text for "What To Pack"
         mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
         Typeface pacificoFont = Typeface.createFromAsset(getAssets(), "fonts/pacifico.ttf");
         mAppNameTextView.setTypeface(pacificoFont);
     }
+
+
 }
