05a4d9c6752060b3b798bba1516f9422005ec981
Mick Sexton <lacrookedbeat@hotmail.com>
2017-09-15 13:08:52 -0700

    add recyclerview for forecast list items on results activity, image and recommendation are hard-coded for now but date and weather conditions are pulled from dailyforecast object

--- app/src/main/java/compioneerx1/github/whattopack/ui/ResultsActivity.java
+++ app/src/main/java/compioneerx1/github/whattopack/ui/ResultsActivity.java
@@ -3,6 +3,8 @@ package compioneerx1.github.whattopack.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -13,6 +15,7 @@ import java.util.ArrayList;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import compioneerx1.github.whattopack.R;
+import compioneerx1.github.whattopack.adapters.ForecastListAdapter;
 import compioneerx1.github.whattopack.models.DailyForecast;
 import compioneerx1.github.whattopack.services.OpenWeatherService;
 import okhttp3.Call;
@@ -25,6 +28,9 @@ public class ResultsActivity extends AppCompatActivity {
 
     @Bind(R.id.inputSummaryTextView) TextView mInputSummaryTextView;
     @Bind(R.id.forecastListView) ListView mForecastListView;
+
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    private ForecastListAdapter mAdapter;
 
     public ArrayList<DailyForecast> mForecasts = new ArrayList<>();
 
@@ -62,12 +68,13 @@ public class ResultsActivity extends AppCompatActivity {
 
                     @Override
                     public void run() {
-                        String[] forecastPreviews = new String[mForecasts.size()];
-                        for (int i = 0; i < forecastPreviews.length; i++) {
-                            forecastPreviews[i] = mForecasts.get(i).getDate();  // might need more than date here, conditions?
-                        }
-                        ArrayAdapter adapter = new ArrayAdapter(ResultsActivity.this, android.R.layout.simple_list_item_1, forecastPreviews);
-                        mForecastListView.setAdapter(adapter);
+
+                        mAdapter = new ForecastListAdapter(getApplicationContext(), mForecasts);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ResultsActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+
                     }
                 });
 
