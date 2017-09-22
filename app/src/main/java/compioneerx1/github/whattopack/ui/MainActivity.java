cfcf81d3d5bddd13453464a3cb4c26b1757a07d9
Mick Sexton <lacrookedbeat@hotmail.com>
2017-09-22 15:24:02 -0700

    add actual login functionality to LoginActivity along with some validation

--- app/src/main/java/compioneerx1/github/whattopack/ui/MainActivity.java
+++ app/src/main/java/compioneerx1/github/whattopack/ui/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if (user != null) {
                     getSupportActionBar().setTitle("Hi there, " + user.getDisplayName() + "!");
                 } else {
-
+                    // nothing
                 }
             }
         };
