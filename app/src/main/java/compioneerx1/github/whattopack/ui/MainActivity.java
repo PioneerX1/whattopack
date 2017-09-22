c0ea77a0d918fe93a96b8c7b60b9d01dfbfb6942
Mick Sexton <lacrookedbeat@hotmail.com>
2017-09-22 14:44:09 -0700

    add log out functionality

--- app/src/main/java/compioneerx1/github/whattopack/ui/MainActivity.java
+++ app/src/main/java/compioneerx1/github/whattopack/ui/MainActivity.java
@@ -6,10 +6,15 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -52,6 +57,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mStartButton.setOnClickListener(this);
         mGithubButton.setOnClickListener(this);
 
+    }
+
+    // menu functions for logging out
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 
 
